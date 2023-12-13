75a2f5036c52e52e91fb81d8b10f861a1ebfd635
Mk9652 <moka1125@gmail.com>
2023-12-13 11:12:47 +0900

    Upate Item.java

--- Item.java
+++ Item.java
@@ -1,1 +1,9 @@
-test
+Public class Item extends TangibleAsset {
+	private int itemnumber;
+	public Item (String name, int price, String color, String itemnumber) {
+		super (name, price, color);
+		this.itemnumber = itemnumber;
+	}
+
+	public int getItemnumber() { return this.itemnumber; }
+}
